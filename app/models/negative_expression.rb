1619c68d3a64a121996e0cbdc95d14eae0cf5495
Antônio Roberto <forevertonny@gmail.com>
2015-02-06 00:51:17 -0200

    added url_match to negative_expressions

--- app/models/negative_expression.rb
+++ app/models/negative_expression.rb
@@ -2,4 +2,16 @@ class NegativeExpression < ActiveRecord::Base
   scope :expressions_for, -> (host) {
     where("lower(?) ~* ANY(domains)", host)
   }
+
+  scope :with_path_regex, ->(path) {
+    where("
+      lower(?) ~* ANY(expressions)
+    ", path)
+  }
+
+  def self.url_match? url, parser = ::Crawler::UrlParser
+    uri = parser.parse url
+    expressions = expressions_for uri[:host]
+    expressions.with_path_regex(uri[:path]).exists?
+  end
 end
--- spec/models/negative_expression_spec.rb
+++ spec/models/negative_expression_spec.rb
@@ -24,4 +24,53 @@ RSpec.describe NegativeExpression, :type => :model do
       end
     end
   end
+
+  describe ".with_regex" do
+    subject { NegativeExpression.with_path_regex path }
+    context "get all expressions that match with regex" do
+      let(:path) { '/foo_bar' }
+
+      context "when regex matches" do
+        before do
+          create(:negative_expression,{
+            domains: ['www.example.com'],
+            expressions: ['/fo*']
+          })
+        end
+
+        it { expect(subject.size).to eq(1) }
+      end
+
+      context "when regex don't matches" do
+        before do
+          create(:negative_expression,{
+            domains: ['www.example.com'],
+            expressions: ['/fooo_bar']
+          })
+        end
+        it { expect(subject.size).to eq(0) }
+      end
+    end
+  end
+
+  describe ".url_match?" do
+    subject { NegativeExpression.url_match? url }
+
+    before do
+      create(:negative_expression,{
+        domains: ['www.example.com'],
+        expressions: ['/bad*']
+      })
+    end
+
+    context "when url matches with a negative expression" do
+      let(:url) { 'www.example.com/bad_url-foo' }
+      it { is_expected.to be_truthy }
+    end
+
+    context "when url don't matches with a negative expression" do
+      let(:url) { 'www.example.com/good_url' }
+      it { is_expected.to be_falsy }
+    end
+  end
 end
