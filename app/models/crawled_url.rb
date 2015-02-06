7362849c050d06919c2021fd5a70716dbe667ce8
Antônio Roberto <forevertonny@gmail.com>
2015-02-06 00:00:24 -0200

    create URL_PARSER cons

--- app/models/crawled_url.rb
+++ app/models/crawled_url.rb
@@ -1,13 +1,15 @@
 class CrawledUrl < ActiveRecord::Base
-  def self.find_for url, parser = ::Crawler::UrlParser
+  URL_PARSER = ::Crawler::UrlParser
+
+  def self.find_for url, parser = URL_PARSER
     self.find_by parser.parse(url)
   end
 
-  def self.persist_from url, parser = ::Crawler::UrlParser
+  def self.persist_from url, parser = URL_PARSER
     self.find_or_create_by parser.parse(url)
   end
 
-  def self.already_persisted? url, parser = ::Crawler::UrlParser
+  def self.already_persisted? url, parser = URL_PARSER
     where(parser.parse(url)).exists?
   end
 end
