6ed8e8388788ac6dd678cafeef6363bd52411a23
Antônio Roberto <forevertonny@gmail.com>
2015-02-06 01:00:45 -0200

    can_persist? look on negative expressions too

--- app/services/analyzer/url_checker.rb
+++ app/services/analyzer/url_checker.rb
@@ -1,7 +1,7 @@
 module Analyzer
   class UrlChecker
     def self.can_persist? url
-      !CrawledUrl.already_persisted? url
+      !CrawledUrl.already_persisted?(url) && !NegativeExpression.url_match?(url)
     end
   end
 end
--- spec/services/analyzer/url_checker_spec.rb
+++ spec/services/analyzer/url_checker_spec.rb
@@ -12,7 +12,25 @@ RSpec.describe Analyzer::UrlChecker, :type => :service do
         create(:crawled_url, attributes)
       end
 
-      it { is_expected.to eq(false) }
+      it { is_expected.to be_falsy }
+    end
+
+    context "when url not persisted but is in negative expressions" do
+      let(:url) { 'http://www.foo.bar.com/bad_url' }
+
+      before do
+        create(:negative_expression,{
+          domains: ['www.foo.bar.com'],
+          expressions: ['/bad*']
+        })
+      end
+
+      it { is_expected.to be_falsy }
+    end
+
+    context "when url is not persisted and not have negative expression for he" do
+      let(:url) { 'http://www.foo.bar.com' }
+      it { is_expected.to be_truthy }
     end
   end
 end
